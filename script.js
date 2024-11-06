d6124df68180f4450191040075dcd9e994c0dd0b
Bilol5577 <bilolbektursunov079@gmail.com>
2024-11-06 12:05:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+    let name = prompt('Ismingizni kiriting');
+    console.log('Ism:', name);
+    let secondname = prompt('Familyangizni kiriting');
+    console.log('Familiya:', secondname);
+    let age = prompt('Yoshingizni kiriting');
+    console.log('Yosh:', age);
+    let favouritescience = prompt('Yoqtirgan faningizni yozing');
+    console.log('Yoqtirgan fan:', favouritescience);
+    let district = prompt('Yashaydigan tumaningizni kiriting');
+    console.log('Yashash manzili:', district);
+
+    alert('Malumotlaringizni kiritganingiz uchun rahmat');
